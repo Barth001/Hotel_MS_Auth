dd147b5d6c23a20cf4936bac63c8e7d5be792efc
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-02-20 11:51:58 +0100

    second commit

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ app.use(express.json())
 
 app.use('/api/v1', rootRoute)
 
-mongoose.connect("mongodb://localhost:27017/HMS", {
-   useNewUrlParser: true,
-   useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to the database')
-}).catch(() => {
-    console.log('There was an error connecting to your database')
-});
-
-// mongoose.connect(process.env.MONGODB_URI)
-// .then(() => {
+// mongoose.connect("mongodb://localhost:27017/HMS", {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true
+// }).then(() => {
 //     console.log('Connected to the database')
 // }).catch(() => {
 //     console.log('There was an error connecting to your database')
-// })
+// });
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('Connected to the database')
+}).catch(() => {
+    console.log('There was an error connecting to your database')
+})
 
 const port = process.env.PORT || 8080
 app.listen(port, ()=> {
